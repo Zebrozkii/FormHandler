10c2587d19bd291b3a5cb9b10abfc7fcff4c51f6
Jimmy Zebroski <45669733+Zebrozkii@users.noreply.github.com>
2021-06-03 11:23:51 -0700

    Update ext

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ echo $_GET['FirstName'] . ' ' .$_GET['LastName'];
   }else{//show form
   echo '
   <form action="">
-    First Name: <input type="text" name ="FirstName" /> <br />
-    Last Name: <input type="text" name ="LAstName" /> <br />
+    First Name:<input type="text" name ="FirstName" /> <br />
+    Last Name: <input type="text" name ="LastName" /> <br />
     <input type="submit" />
   </form>
   
